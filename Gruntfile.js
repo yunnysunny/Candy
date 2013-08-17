48a3f84ec2b49120e3b19b4ce4519edcbce5670b
Colin Wren <colinwrenca@gmail.com>
2013-08-17 11:42:32 -0700

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,5 @@
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
-
 module.exports = function (grunt) {
 
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   var gruntPagesConfig = JSON.parse(grunt.template.process(grunt.file.read('cabin.json'), {
       data: {
         templateLang: 'jade'
@@ -12,24 +7,61 @@ module.exports = function (grunt) {
     })).gruntPages;
 
   grunt.initConfig({
+    pages: gruntPagesConfig,
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src/styles',
+          cssDir: 'dist/styles'
+        }
+      }
+    },
+    // Move files not handled by other tasks
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'src',
+          dest: 'dist',
+          src: [
+            'images/**',
+            'scripts/**',
+            'styles/**.css',
+            'styles/fonts/**',
+          ]
+        }]
+      }
+    },
     watch: {
-      options: {
-        livereload: true
+      dist: {
+        files: ['dist/**'],
+        options: {
+          livereload: true
+        }
       },
       compass: {
         files: ['src/styles/**'],
         tasks: ['compass']
       },
       pages: {
-        files: ['src/pages/**', 'posts/**', 'src/layouts/**'],
+        files: [
+          'posts/**',
+          'src/layouts/**',
+          'src/pages/**'
+        ],
         tasks: ['pages']
       },
       copy: {
-        files: ['src/images/**', 'src/styles/**.css', 'src/styles/fonts/**', 'src/scripts/**'],
+        files: [
+          'src/images/**',
+          'src/scripts/**',
+          'src/styles/**.css',
+          'src/styles/fonts/**'
+        ],
         tasks: ['copy']
       }
     },
-    pages: gruntPagesConfig,
     connect: {
       dist: {
         options: {
@@ -38,8 +70,7 @@ module.exports = function (grunt) {
           middleware: function (connect) {
             return [
               require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
-              mountFolder(connect, 'dist'),
-              mountFolder(connect, 'src')
+              connect.static(require('path').resolve('dist'))
             ];
           }
         }
@@ -52,46 +83,22 @@ module.exports = function (grunt) {
     },
     clean: {
       dist: 'dist'
-    },
-    compass: {
-      options: {
-        sassDir: 'src/styles',
-        cssDir: 'dist/styles'
-      },
-      dist: {}
-    },
-    // Move files not handled by other tasks
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: 'src',
-          dest: 'dist',
-          src: [
-            'images/**',
-            'styles/**.css',
-            'styles/fonts/**',
-            'scripts/**'
-          ]
-        }]
-      }
     }
   });
 
   grunt.registerTask('build', [
     'clean',
-    'compass',
     'pages',
+    'compass',
     'copy'
   ]);
 
-  grunt.registerTask('server', [
+  grunt.registerTask('default', [
     'build',
     'connect',
     'open',
     'watch'
   ]);
 
-  grunt.registerTask('default', 'server');
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 };
