fa466b65a667388de65f17e83af0fd380f4c44cc
Colin Wren <colinwrenca@gmail.com>
2013-08-21 16:21:17 -0700

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,22 @@
 $(function() {
-  var nav = false;
+
+  // Follow links on first click on mobile Safari
+  $('a').on('click touchend', function() {
+    window.location = $(this).attr('href');
+  });
+
+  // If touchscreen listen for touch, if not listen for click
+  var hitEvent = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click';
+
   // Show or hide nav dropdown
-  $('body').click(function(event) {
+  var nav = false;
+
+  $(document).on(hitEvent, function(event) {
+
     if (nav) {
       $('nav').css({ overflow: 'hidden' });
       nav = false;
+
     } else if ($(event.target).hasClass('menu')) {
       $('nav').css({ overflow: 'visible' });
       nav = true;
