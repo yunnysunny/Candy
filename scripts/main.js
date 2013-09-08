39eb212822d2dc18d98815b0ab52bda1f61ee214
ChrisWren <cthewren@gmail.com>
2013-09-08 15:24:26 -0700

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
-$(function() {
+$(function () {
+
+  'use strict';
 
   var $navBar = $('nav');
 
   // Toggle the nav menu list when the user clicks the nav menu button
-  $('.menu').on('click', function(event) {
+  $('.menu').on('click', function () {
     if ($navBar.css('overflow') !== 'visible') {
-      $navBar.css('overflow', 'visible');  
+      $navBar.css('overflow', 'visible');
     } else {
       $navBar.css('overflow', 'hidden');
     }
